b1cf35b1395570d4a6a582c8736896eedb12df0a
avinash-vellineni-hertz-com <avinash.vellineni@hertz.com>
2021-01-16 13:42:46 -0600

    Array leetcode problems

---
+++ Java/Leetcode/ProblemSolvings/src/main/java/com/problems/arrays/ThreeSumSequential.java
@@ -0,0 +1,29 @@
+package com.problems.arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ThreeSumSequential {
+    public static void main(String[] args){
+        int[] numbers = new int[] {0};
+        System.out.println(threeSum(numbers));
+    }
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<Integer> inputList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        inputList.sort((e1,e2) -> e1.compareTo(e2));
+        List<List<Integer>> output = new ArrayList<>();
+        System.out.println(inputList);
+        for(int i=0;i<inputList.size()-3;i++){
+            List<Integer> eachTemporaryOutput = new ArrayList<>();
+            if(inputList.get(i) + inputList.get(i+1) + inputList.get(i+2) == 0){
+                eachTemporaryOutput.add(inputList.get(i));
+                eachTemporaryOutput.add(inputList.get(i+1));
+                eachTemporaryOutput.add(inputList.get(i+2));
+                output.add(eachTemporaryOutput);
+            }
+        }
+        return output;
+    }
+}
