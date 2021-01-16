971b8c99108c93cc5e52931cbf149e1b35c30c41
avinash-vellineni-hertz-com <avinash.vellineni@hertz.com>
2021-01-16 13:09:30 -0600

    Array leetcode problems

---
+++ Java/Leetcode/ProblemSolvings/src/main/java/com/problems/arrays/MaxContainerArea.java
@@ -0,0 +1,22 @@
+package com.problems.arrays;
+
+public class MaxContainerArea {
+    public static void main(String[] args){
+        int[] arrayHeights = new int[] {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(arrayHeights));
+    }
+
+    public static int maxArea(int[] height) {
+        int maximumArea = 0,start = 0;
+        int end = height.length -1;
+        while(start < end){
+            maximumArea = Math.max(maximumArea,(Math.min(height[start],height[end])) * (end-start) );
+            if(height[start]<height[end]){
+                start++;
+            }else {
+                end--;
+            }
+        }
+        return maximumArea;
+    }
+}
---
+++ Java/Leetcode/ProblemSolvings/src/main/java/com/problems/arrays/ThreeSum.java
@@ -0,0 +1,12 @@
+package com.problems.arrays;
+
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args){
+
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        return null;
+    }
+}
